1857079b4aa0eb92f373a2a0f8b3f9d53aed2944
soad <mdsolimansoad100099@gmail.com>
2022-09-29 01:31:45 +0600

    header font awesome icon added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,15 @@
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Header.css';
 
 const Header = () => {
+    const element = <FontAwesomeIcon icon={faDumbbell} />
+
     return (
-        <div>
+        <div className='header'>
                        
-            <h1><FontAwesomeIcon icon={['fab', 'apple']} />Fitness club</h1>
+            <h1> {element} Fitness club</h1>
         </div>
     );
 };
