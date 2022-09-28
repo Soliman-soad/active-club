c61f8f75982a90fcb9c58d576db8e06b5d9b5fba
soad <mdsolimansoad100099@gmail.com>
2022-09-29 02:08:31 +0600

    footer section added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Question from './components/Question/Question';
@@ -9,6 +10,7 @@ function App() {
       <Header></Header>
       <Main></Main>
       <Question></Question>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <p>©All right reserved by MD Soliman Alam</p>
+        </div>
+    );
+};
+
+export default Footer;
