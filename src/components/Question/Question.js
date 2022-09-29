fdbf3f67db7bebf4762248738bbe479a59fdac09
soad <mdsolimansoad100099@gmail.com>
2022-09-29 08:51:16 +0600

    title name change

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -4,6 +4,7 @@ import './Question.css';
 const Question = () => {
     return (
         <div className='question-container'>
+            <h2>Question</h2>
             <div>
                 <h4>How react work ?</h4>
                 <p>React code look a like html but not html.Its jsx format which mainly compailed by bable.ReactJS is an open-source, component-based front end library responsible only for the view layer of the application.</p>
@@ -13,8 +14,8 @@ const Question = () => {
                 <p>Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components.</p>
             </div>
             <div>
-                <h4>How useState work?</h4>
-                <p> The  useState hook is a special function that takes the initial state as an argument and returns an array of two entries.  UseState encapsulate only singular value from the state, for multiple state need to have useState calls.</p>
+                <h4> useEffect uses</h4>
+                <p>useEffect use to render data . It has dependency array which value change helps to render initialy and t manages the side-effects in functional components. callback argument is a function to put the side-effect logic </p>
             </div>
         </div>
     );
